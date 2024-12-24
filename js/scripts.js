9aa3f5b13b77f6bb91e47a4af203338a90513eb6
Kieran-Go <187461911+Kieran-Go@users.noreply.github.com>
2024-12-25 07:20:30 +1030

    Display books in a table

--- js/scripts.js
+++ js/scripts.js
@@ -14,7 +14,34 @@ function addBookToLibrary(name, author, pages, read){
     myLibrary.push(book);
 }
 
+function populateLibraryTable(){
+    const libraryTable = document.querySelector(".table-body");
+    for(let i = 0; i < myLibrary.length; i++){
+        const book = myLibrary[i];
+        libraryTable.innerHTML +=
+        `<tr>
+            <td>${book.name}</td>
+            <td>${book.author}</td>
+            <td>${book.pages}</td>
+            <td>${book.read}</td>
+        </tr>`
+    }
+}
+
+// Initialize the library
 const myLibrary = [];
 
+// Populate the library with books
 addBookToLibrary("The Hobbit", "J.R.R Tolkien", 295, false);
-console.log(myLibrary[0].info());
+addBookToLibrary("To Kill a Mockingbird", "Harper Lee", 281, true);
+addBookToLibrary("1984", "George Orwell", 328, false);
+addBookToLibrary("Pride and Prejudice", "Jane Austen", 279, true);
+addBookToLibrary("Moby-Dick", "Herman Melville", 635, false);
+addBookToLibrary("The Great Gatsby", "F. Scott Fitzgerald", 180, true);
+addBookToLibrary("War and Peace", "Leo Tolstoy", 1225, false);
+addBookToLibrary("Crime and Punishment", "Fyodor Dostoevsky", 671, true);
+addBookToLibrary("The Catcher in the Rye", "J.D. Salinger", 214, false);
+addBookToLibrary("Brave New World", "Aldous Huxley", 268, true);
+
+populateLibraryTable();
+
